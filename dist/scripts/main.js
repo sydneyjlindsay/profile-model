a9bc7d7195f0fe3751de6de2af0234a28c75fd9b
Sydney Lindsay <sydneyjlindsay@gmail.com>
2015-06-08 22:56:43 -0500

    normal mode finished

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,44 @@
-var user = new UserModel();
-var App = Backbone.Router.extend({
-	routes: {
-		'': 'profile',
-		'edit': 'edit'
-	},
-	profile: function() {
-		$('.page').hide();
-		$('#profile').show();
-	},
-	edit: function() {
-		$('.page').hide();
-		$('#edit').show();
+$(document).ready(function() {
+	
+	var App = Backbone.Router.extend({
+		routes: {
+			'': 'profile',
+			'edit': 'edit', 
+		},
+		profile: function() {
+			$('.page').hide();
+			$('#profile').show();
+		},
+		edit: function() {
+			$('.page').hide();
+			$('#edit').show();
+		}
+	});
+
+	var app = new App();
+	Backbone.history.start();
+
+	var user = new UserModel();
+
+	user.on('change', updateProfile);
+
+	function updateProfile(UserModel) {
+		$('.profile-usertitle-name').html(UserModel.get('name'));
+		$('.profile-usertitle-job').html(UserModel.get('role'));
+		$('.dropdown-toggle').html(UserModel.get('name'));
 	}
+
+	$('#edit form').on('submit', function(e) {
+		e.preventDefault();
+		user.set({
+			name: $('#name').val(),
+			role: $('#role').val()
+		});
+		
+		$('#name').val('');
+		$('#role').val('');
+	});
+
 });
 
-var app = new App();
-Backbone.history.start();
+
